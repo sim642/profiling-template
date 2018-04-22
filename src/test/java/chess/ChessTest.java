7c6a943b41b24604771c096f6be15a0636606f93
Märt Bakhoff <mart.bakhoff@gmail.com>
2018-04-22 13:21:57 +0300

    faster tests

--- src/test/java/chess/ChessTest.java
+++ src/test/java/chess/ChessTest.java
@@ -12,19 +12,19 @@ public class ChessTest {
     public void testUnreachableBoards() {
         Square start = new Square(0, 1);
         Square destination = new Square(4, 4);
-        Assert.assertFalse(App.isReachable(112, start, destination));
-        Assert.assertFalse(App.isReachable(108, start, destination));
-        Assert.assertFalse(App.isReachable(104, start, destination));
-        Assert.assertFalse(App.isReachable(100, start, destination));
+        Assert.assertFalse(App.isReachable(32, start, destination));
+        Assert.assertFalse(App.isReachable(28, start, destination));
+        Assert.assertFalse(App.isReachable(24, start, destination));
+        Assert.assertFalse(App.isReachable(20, start, destination));
     }
 
     @Test
     public void testReachableBoards() {
         Square start = new Square(0, 1);
-        Square destination = new Square(62, 40);
-        Assert.assertTrue(App.isReachable(112, start, destination));
-        Assert.assertTrue(App.isReachable(108, start, destination));
-        Assert.assertTrue(App.isReachable(104, start, destination));
-        Assert.assertTrue(App.isReachable(100, start, destination));
+        Square destination = new Square(4, 5);
+        Assert.assertTrue(App.isReachable(32, start, destination));
+        Assert.assertTrue(App.isReachable(28, start, destination));
+        Assert.assertTrue(App.isReachable(24, start, destination));
+        Assert.assertTrue(App.isReachable(20, start, destination));
     }
 }
